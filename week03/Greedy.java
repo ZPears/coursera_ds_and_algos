4647668247337f7c95b11d2eeeb039b97d610810
Zach <zachary.p.pearson@gmail.com>
2016-10-03 23:12:32 -0400

    greedy intro lectures complete

--- week03/Greedy.java
+++ week03/Greedy.java
@@ -5,4 +5,46 @@ import java.io.*;
 
 public class Greedy {
 	
+	public static void main(String[] args) {
+		
+		System.out.println(greedyMinRefills());
+
+	}
+	
+	// route \x dist on tank, a is starting point, b is end point,
+	// route is an array of gas station points in kms, where starting point = route[0] and end = route[n+1]
+	static int greedyMinRefills(int[] route, int l) {
+	
+		// initialize two variables to hold refill count and current refill
+		int numRefills = 0;
+		int currentRefill = 0;
+		int lastRefill = 0;
+
+		// 
+		while (currentRefill <= n) {
+			
+			lastRefill = currentRefill;
+			
+			while (currentRefill <= n && x[currentRefill + 1] - x[lastRefill] <= l) {
+
+				currentRefill = currentRefill + 1
+
+			}
+
+			if (currentRefill == lastRefill) {
+
+				throw new Error();			
+
+			}
+
+			if (currentRefill <= n):
+
+				numRefills = numRefills + 1		
+
+			}
+		}
+
+		return numRefills;
+	}
+
 }
