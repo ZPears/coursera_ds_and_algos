38dde48f928dffc97b5a485ee385dc02799f6fd6
Zach <zachary.p.pearson@gmail.com>
2016-10-30 21:55:08 -0400

    finished old week02 stuff

--- week02/homework/FibonacciLastDigit.java
+++ week02/homework/FibonacciLastDigit.java
@@ -7,7 +7,7 @@ public class FibonacciLastDigit {
         long[] fibs = new long[n+1];
         fibs[0] = 0; fibs[1] = 1;
         for (int i = 2; i <= n; i++) {
-            fibs[i] = fibs[i - 1] + fibs[i - 2];
+            fibs[i] = (fibs[i - 1] + fibs[i - 2]) % 10;
         }
         return fibs[n] % 10;
     }
