7ce5d0365b660e89353de98bc403f223eb783321
Zachary Pearson <zachary.p.pearson@gmail.com>
2016-11-06 14:02:23 -0500

    finished alignment lectures

---
+++ week05/AlignmentGame.java
@@ -0,0 +1,117 @@
+import java.util.*;
+import java.io.*;
+import java.lang.Math;
+
+public class AlignmentGame {
+
+	public static void main(String[] args) {
+
+		String a = "bread";
+		String b = "really";
+
+		int[][] result = editDistance(a,b);
+
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(Arrays.toString(result[i]));
+		}
+
+		String[] ans = outputAlignment(a,b,result);
+
+		for (int i = 0; i < ans.length; i++) {
+			System.out.println(ans[i]);
+		}
+
+	}
+
+	public static int[][] editDistance(String a, String b) {
+
+		int[][] d = new int[a.length()+1][b.length()+1];
+
+		for (int i = 0; i < a.length()+1; i++) {
+
+			d[i][0] = i;
+
+			if (i == 0) {
+
+				for (int j = 0; j < b.length()+1; j++) {
+
+					d[i][j] = j;
+
+				}
+
+			}
+
+		}
+
+		for (int j = 1; j <= b.length(); j++) {
+
+			for (int i = 1; i <= a.length(); i++) {
+
+				int match = 0;
+
+				// blue color in lecture
+				int insertion = d[i][j-1] + 1;
+				// green color in lecture
+				int deletion = d[i-1][j] + 1;
+				if (a.charAt(i-1) == b.charAt(j-1) ) {
+					// purple color in lecture
+					match = d[i-1][j-1];
+				} else {
+					// orange color in lecture
+					match = d[i-1][j-1] + 1;
+				}
+
+				int ans = Math.min( Math.min(insertion, deletion), match ); 
+
+				d[i][j] = ans;
+
+			}
+
+
+		}
+
+		return d;
+
+	}
+
+	public static String[] outputAlignment(String a, String b, int[][] d) {
+
+		int i = a.length();
+		int j = b.length();
+
+		StringBuilder aBuf = new StringBuilder();
+		StringBuilder bBuf = new StringBuilder();
+
+		while (i > 1 && j > 1) {
+
+			if (i > 0 && d[i][j] == d[i-1][j] + 1) {
+
+				i--;
+				aBuf.insert(0, a.charAt(i-1));
+				bBuf.insert(0, "-");
+
+			} else if (j > 0 && d[i][j] == d[i][j-1] + 1) {
+
+				j--;
+				aBuf.insert(0, "-");
+				bBuf.insert(0, b.charAt(j-1));
+
+			} else {
+
+				i--; j--;
+				aBuf.insert(0, a.charAt(i-1));
+				bBuf.insert(0, b.charAt(j-1));
+
+			}
+
+		}
+
+		String[] result = new String[2];
+		result[0] = aBuf.toString();
+		result[1] = bBuf.toString();
+
+		return result;
+
+	}
+
+}
---
+++ week05/ChangeProblem.java
@@ -0,0 +1,107 @@
+import java.util.*;
+import java.io.*;
+
+public class ChangeProblem {
+
+	public static void main(String[] args) {
+
+		int[] coins = {20,8,1};
+
+		System.out.println(greedyChange(24, coins).toString());
+
+		System.out.println(recursiveChange(24, coins));
+
+		System.out.println(Arrays.toString(dynamicChange(24, coins)));
+
+	}
+
+	public static ArrayList greedyChange(int amount, int[] coins) {
+
+		ArrayList result = new ArrayList();
+
+		int coinIdx = 0;
+		int currDenom = coins[coinIdx];
+
+		while (amount > 0) {
+
+			if (currDenom <= amount) {
+
+				result.add(currDenom);
+				amount -= currDenom;
+
+			} else {
+
+				coinIdx += 1;
+				currDenom = coins[coinIdx];
+
+			}
+
+		}
+
+		return result;
+
+	}
+
+	public static int recursiveChange(int amount, int[] coins) {
+
+		if (amount == 0) {
+			return 0;
+		}
+
+		int minNumCoins = 100000;
+
+		for (int i = 0; i < coins.length; i++) {
+
+			// crappy and takes forever, since every one makes n coins calls
+
+			if (amount >= coins[i]) {
+
+				int numCoins = recursiveChange(amount - coins[i], coins);
+
+				if (numCoins + 1 < minNumCoins) {
+
+					minNumCoins = numCoins + 1;
+
+				}
+
+			} 
+
+		}
+
+		return minNumCoins;
+
+	}
+
+	public static int[] dynamicChange(int amount, int[] coins) {
+
+		int numCoins = 0;
+
+		int[] minNumCoins = new int[amount+1];
+
+		for (int m = 1; m <= amount; m++) {
+
+			minNumCoins[m] = 10000;
+
+			for (int i = 0; i < coins.length; i++) {
+
+				if (m >= coins[i]) {
+
+					numCoins = minNumCoins[m - coins[i]] + 1;
+
+					if (numCoins < minNumCoins[m]) {
+
+						minNumCoins[m] = numCoins;
+
+					}
+
+				}
+
+			}
+
+		}
+
+		return minNumCoins;
+
+	}
+
+}
